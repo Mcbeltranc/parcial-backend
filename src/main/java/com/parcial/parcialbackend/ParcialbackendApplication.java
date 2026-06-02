f5d0d13566f6260a3213f8098f7f05881c8088bb
camila <beltrancarrenomariacamila@gmail.com>
2026-06-02 12:12:16 -0500

    Initial commit

---
+++ src/main/java/com/parcial/parcialbackend/ParcialbackendApplication.java
@@ -0,0 +1,13 @@
+package com.parcial.parcialbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialbackendApplication.class, args);
+	}
+
+}
